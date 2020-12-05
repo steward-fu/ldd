53395459f49771532e0cbf70513dd538e1f9f9e8
steward-fu <steward.fu@gmail.com>
2020-12-05 06:23:52 -0500

    add gpio_input

--- cpp/gpio_input/main.c
+++ cpp/gpio_input/main.c
@@ -11,6 +11,7 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Steward Fu");
 MODULE_DESCRIPTION("Linux Driver");
  
+#define BUTTON   27
 #define USR3_LED ((32 * 1) + 24)
 
 static int period=100;
@@ -18,9 +19,9 @@ static struct timer_list timer={0};
 
 void timer_handler(unsigned long unused)
 {
-  static int state=0;
+  int state=0;
      
-  state^= 1;
+  state = gpio_get_value(BUTTON);
   gpio_set_value(USR3_LED, state);
   mod_timer(&timer, jiffies + msecs_to_jiffies(period));
 }
@@ -29,6 +30,10 @@ int ldd_init(void)
 {
   gpio_request(USR3_LED, "USR3");
   gpio_direction_output(USR3_LED, 1);
+
+  gpio_request(BUTTON, "BTN");
+  gpio_direction_input(BUTTON);
+  
   setup_timer(&timer, timer_handler, 0);
   mod_timer(&timer, jiffies + msecs_to_jiffies(period));
   return 0;
@@ -37,6 +42,7 @@ int ldd_init(void)
 void ldd_exit(void)
 {
   del_timer(&timer);
+  gpio_free(BUTTON);
   gpio_free(USR3_LED);
 }
  
