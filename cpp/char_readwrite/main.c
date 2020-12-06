2675a6fdd15a8cd3718733b48f2ef1cf4b56973a
steward-fu <steward.fu@gmail.com>
2020-12-06 04:34:25 -0500

    add chrdev_readwrite

--- cpp/char_readwrite/main.c
+++ cpp/char_readwrite/main.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/module.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Steward Fu");
@@ -10,6 +11,7 @@ MODULE_DESCRIPTION("Linux Driver");
 static int base=0;
 static struct cdev mycdev;
 static char mybuf[255]={0};
+static struct class *myclass=NULL;
 
 static int myopen(struct inode *inode, struct file *file)
 {
@@ -25,12 +27,20 @@ static int myclose(struct inode *inode, struct file *file)
 
 static ssize_t myread(struct file *flip, char __user *buf, size_t len, loff_t *off)
 {
-  return copy_to_user(buf, mybuf, strlen(mybuf));
+  int r=0;
+  
+  r = copy_to_user(buf, mybuf, strlen(mybuf));
+  printk("%s, %s\n", __func__, mybuf);
+  return r;
 }
 
 static ssize_t mywrite(struct file *flip, const char __user *buf, size_t len, loff_t *off)
 {
-  return copy_from_user(mybuf, buf, len);
+  int r=0;
+
+  r = copy_from_user(mybuf, buf, len);
+  printk("%s, %s\n", __func__, mybuf);
+  return r;
 }
 
 static const struct file_operations myfops = {
@@ -44,15 +54,18 @@ static const struct file_operations myfops = {
 int ldd_init(void)
 {
   alloc_chrdev_region(&base, 0, 1, "myfile");
+  myclass = class_create(THIS_MODULE, "myfile");
+  device_create(myclass, NULL, base, NULL, "myfile");
   cdev_init(&mycdev, &myfops);
   cdev_add(&mycdev, base, 1);
-  printk("major:%d, minor:%d\n", MAJOR(base), MINOR(base));
   return 0;
 }
  
 void ldd_exit(void)
 {
+  device_destroy(myclass, base);
   cdev_del(&mycdev);
+  class_destroy(myclass);
   unregister_chrdev_region(base, 1);
 }
  
